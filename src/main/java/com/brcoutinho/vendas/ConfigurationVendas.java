f64ee0fa4ee3f9d12a14fec10817adad37dc4afc
Thiago Coutinho <thcoutinho1997@gmail.com>
2021-04-26 16:20:31 -0300

    Configuração - application.properties

--- src/main/java/com/brcoutinho/vendas/ConfigurationVendas.java
+++ src/main/java/com/brcoutinho/vendas/ConfigurationVendas.java
@@ -5,8 +5,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigurationVendas {
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sistema de Vendas";
-    }
+
 }
