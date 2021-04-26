5ed8fe295ff98e6fe44562510dfbde0d542d21e4
Thiago Coutinho <thcoutinho1997@gmail.com>
2021-04-26 16:42:26 -0300

    Trabalhando com múltiplos ambientes e profiles

--- src/main/java/com/brcoutinho/vendas/ConfigurationVendas.java
+++ src/main/java/com/brcoutinho/vendas/ConfigurationVendas.java
@@ -1,9 +1,17 @@
 package com.brcoutinho.vendas;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development") //o que estiver nessa classe só rodará para o perfil 'development'
 public class ConfigurationVendas {
-
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Rodando a configuração de desenvolvimento");
+        };
+    }
 }
